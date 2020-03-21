b02b32f3fad31d09f44e5955096a2fb6e5549490
angatupyry <fierrofenix@gmail.com>
2020-03-21 15:07:11 -0300

    Add create User

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,27 +9,46 @@ class UserService {
   }
 
   getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + 'user', {
+      headers: authHeader()
+    });
   }
 
   getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
+    return axios.get(API_URL + 'mod', {
+      headers: authHeader()
+    });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    return axios.get(API_URL + 'admin', {
+      headers: authHeader()
+    });
   }
-  
+
   listUsers() {
-    return axios.get(API_URL + 'listUsers', { headers: authHeader() });
+    return axios.get(API_URL + 'listUsers', {
+      headers: authHeader()
+    });
   }
 
-  createUser() {
-    return axios.post(API_URL + 'createUser', { headers: authHeader() });
-  }
-
-  listRoles() {
-    return axios.get(API_URL + 'roles', { headers: authHeader() });
+  createUser(user) {
+    return axios
+      .post(API_URL + 'createUser', {
+        nombre: user.nombre,
+        apellido: user.apellido,
+        email: user.email,
+        rol_id: user.rol_id,
+        username: user.username,
+        password: user.password
+      })
+      .then(response => {
+        //TODO CONSUMIR TOKEN DEL API Y UTILIZAR PARA LAS RUTAS
+        // if (response.data.accessToken) {
+        //   localStorage.setItem('user', JSON.stringify(response.data));
+        // }
+        return response.data;
+      });
   }
 
 }
