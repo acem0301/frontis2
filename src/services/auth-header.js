5e9d56f7e66353cbd6dc5834bce137a907c9a900
angatupyry <fierrofenix@gmail.com>
2020-03-29 19:30:03 -0400

    fix

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,8 +2,9 @@ export default function authHeader() {
   let user = JSON.parse(localStorage.getItem('user'));
 
   if (user && user.token) {
-    return { Authorization: 'Bearer ' + user.token }; 
-    // return { 'x-access-token': user.accessToken };       // Consumir del backend. 
+    return {
+      Authorization: 'Bearer ' + user.token
+    };
   } else {
     return {};
   }
