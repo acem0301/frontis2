e1b67ea196e7604ae8f4bbedf5818a168c42cdbe
Angatupyry <fierrofenix@gmail.com>
2020-09-06 21:20:01 -0400

    Fix dev

--- src/services/rol.service.js
+++ src/services/rol.service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://backendis2.herokuapp.com/';
-//const API_URL = 'http://localhost:3000/';
+//const API_URL = 'https://backendis2.herokuapp.com/';
+const API_URL = 'http://localhost:3000/';
 class RolService {
   getPublicContent() {
     return axios.get(API_URL + 'all');
