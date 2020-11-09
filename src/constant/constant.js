7694a3019b00c079edf2ce1581383e69793250fa
Angatupyry <fierrofenix@gmail.com>
2020-11-09 15:47:23 -0300

    Desa

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://backendis2.herokuapp.com/';
+//const API_URL = 'https://backendis2.herokuapp.com/';
 
-//const API_URL = 'http://localhost:3000/'
+const API_URL = 'http://localhost:3000/'
 
 export default {
     API_URL: API_URL
